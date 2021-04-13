55e1eff3d6f94a437e7fdcd8493c7361f74593cd
rafaelle <rafaelle.xavier@gmail.com>
2021-04-12 21:53:03 -0300

    feat: adding header

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { User } from '../../models/user/user';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass']
+})
+export class HeaderComponent implements OnInit {
+  @Input() user!: User;
+
+  constructor(
+    private authService: AuthService,
+  ) { }
+
+  ngOnInit(): void {
+    this.authService.getUser() ? this.user = new User(this.authService.getUser()) : this.user = new User();
+    ;
+  }
+
+}
